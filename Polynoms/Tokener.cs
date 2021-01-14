afa87963e629eac6a518aa78a2144615f1d8c806
saltytimofey <saltytimofey@gmail.com>
2021-01-14 12:40:52 +0700

    Написан класс Tokener

--- Polynoms/Tokener.cs
+++ Polynoms/Tokener.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace Polynoms
 {
-    class Tokener
+    ///Класс, отвечающий за токены и перевод строки в токены
+    public abstract class Tokener
     {
+        ///Набор состояний токенера
+        private enum TokenerState
+        {
+            Start,
+            Error,
+            End,
+            Operation,
+            Operand,
+            OperandAftDot,
+            Polynom,
+            Warning,
+            Dot
+        }
+
+        ///Набор типов токена
+        public enum TokenType
+        {
+            Operation = 0,
+            Number = 1,
+            Variable = 2
+        }
+
+        ///Значение и тип токена
+        public struct Token
+        {
+            public string Value;
+            public TokenType Type;
+        }
+
+        /**
+         * Проверяет, является ли символ переменной
+         * \param v Переменная, с которой необходимо сравнивать
+         * \param z Символ, который необходимо проверять
+         * \return True, если z является переменной и False, если нет
+         */
+        private static bool IsVar(char? v, char z)
+        {
+            return v == null || v == z;
+        }
+
+        /**
+         * Проверяет, является ли символ операцией
+         * \param z Символ, который необходимо проверять
+         * \return True, если z является операцией и False, если нет
+         */
+        private static bool IsOp(char z)
+        {
+            return z == '+' || z == '-' || z == '*' || z == '/' || z == '^';
+        }
     }
 }
+
