759c6f12295c1f58c0159dc82894bcb149f10ba0
saltytimofey <saltytimofey@gmail.com>
2021-01-14 12:54:25 +0700

    Добавлены базовые функции для класса Calculator

--- Polynoms/Calculator.cs
+++ Polynoms/Calculator.cs
@@ -15,6 +15,66 @@ namespace Polynoms
             Warning = 2  
         }
 
+        private static void Calculate(IList<Tokener.Token> tokens, int index, CalcDelegate mathAction)
+        {
+            tokens[index] = mathAction(tokens[index - 1], tokens[index + 1]);
+            tokens.RemoveAt(index + 1);
+            tokens.RemoveAt(index - 1);
+        }
+
+        private static Tokener.Token Add(Tokener.Token x, Tokener.Token y)
+        {
+            return new Tokener.Token
+            {
+                Value = Math.Round(double.Parse(x.Value) + double.Parse(y.Value), 3).ToString(),
+                Type = Tokener.TokenType.Number
+            };
+        }
+
+        private static Tokener.Token Substract(Tokener.Token x, Tokener.Token y)
+        {
+            return new Tokener.Token
+            {
+                Value = Math.Round(double.Parse(x.Value) - double.Parse(y.Value), 3).ToString(),
+                Type = Tokener.TokenType.Number
+            };
+        }
+
+        private static Tokener.Token Multiply(Tokener.Token x, Tokener.Token y)
+        {
+            return new Tokener.Token
+            {
+                Value = Math.Round(double.Parse(x.Value) * double.Parse(y.Value), 3).ToString(),
+                Type = Tokener.TokenType.Number
+            };
+        }
+
+        private static Tokener.Token Divide(Tokener.Token x, Tokener.Token y)
+        {
+            return new Tokener.Token
+            {
+                Value = Math.Round(double.Parse(x.Value) / double.Parse(y.Value), 3).ToString(),
+                Type = Tokener.TokenType.Number
+            };
+        }
+
+        private static Tokener.Token Pow(Tokener.Token x, Tokener.Token y)
+        {
+            return new Tokener.Token
+            {
+                Value = Math.Round(Math.Pow(double.Parse(x.Value), double.Parse(y.Value)), 3).ToString(),
+                Type = Tokener.TokenType.Number
+            };
+        }
+
+        private static double Pow(double x, double y)
+        {
+            return Math.Round(Math.Pow(x, y), 3);
+        }
+
+        private delegate Tokener.Token CalcDelegate(Tokener.Token x, Tokener.Token y);
+
+         
         private struct PolynomPow
         {
             public double Power;
