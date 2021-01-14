880d8fb02b55adfb7413ec3aa5b2dc2f49fdee51
saltytimofey <saltytimofey@gmail.com>
2021-01-14 12:53:28 +0700

    Созданы структуры и enum для класса Calcu;ator

--- Polynoms/Calculator.cs
+++ Polynoms/Calculator.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Polynoms
 {
-    class Calculator
+    public abstract class Calculator
     {
+        public enum AnswerCode
+        {
+            Ok = 0,  
+            Error = 1,  
+            Warning = 2  
+        }
+
+        private struct PolynomPow
+        {
+            public double Power;
+            public double Value;
+        }
+
+         
+        public struct Answer
+        {
+            public string Ans;
+            public AnswerCode Code;
+        }
     }
 }
